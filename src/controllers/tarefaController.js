f00a60e6daa465c06eb20a5a5c59d3c049cefbb6
Sara Souza <sarasouza.tds2.senai@gmail.com>
2025-03-11 16:13:27 -0300

    feat add create method

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -5,21 +5,25 @@ class TarefaController {
       const tarefas = await tarefaModel.getAll();
       res.json(tarefas);
     } catch (error) {
-     console.error(error);
-     res.status(500).json({ erro: "Não foi possível buscar as tarefas" });
+      console.error(error);
+      res.status(500).json({ erro: "Erro ao buscar as tarefas" });
     }
-
-
-
-    
   };
-  create = ({ body: { descricao } }, res) => {
-    if (!descricao) {
-      return res.status(400).json({ erro: "Descrição é obrigatória" });
+
+  create = async (req, res) => {
+    const { descricao } = req.body;
+    try {
+      if (!descricao) {
+        return res.status(400).json({ erro: "Descrição é obrigatória" });
+      }
+      const novaTarefa = await tarefaModel.create(descricao);
+      res.status(201).json(novaTarefa);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ erro: "Erro ao criar a tarefa" });
     }
-    const novaTarefa = tarefaModel.create(descricao);
-    res.status(201).json(novaTarefa);
   };
+
   update = ({ params: { id }, body: { concluida } }, res) => {
     const tarefaAtualizada = tarefaModel.update(id, concluida);
     if (!tarefaAtualizada) {
@@ -27,6 +31,7 @@ class TarefaController {
     }
     res.json(tarefaAtualizada);
   };
+  
   delete = ({ params: { id } }, res) => {
     const sucesso = tarefaModel.delete(id);
     if (!sucesso) {
@@ -36,3 +41,6 @@ class TarefaController {
   };
 }
 export default new TarefaController();
+
+
+
