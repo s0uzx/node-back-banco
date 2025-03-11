bd42c7e41148c2eccfcd72b91b725eb24d70f07b
Sara Souza <sarasouza.tds2.senai@gmail.com>
2025-03-11 15:39:08 -0300

    feat: adjust get all method

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -1,8 +1,17 @@
 import tarefaModel from "../models/tarefaModel.js";
 class TarefaController {
-  getAll = (req, res) => {
-    const tarefas = tarefaModel.getAll();
-    res.json(tarefas);
+  getAll = async (req, res) => {
+    try {
+      const tarefas = await tarefaModel.getAll();
+      res.json(tarefas);
+    } catch (error) {
+     console.error(error);
+     res.status(500).json({ erro: "Não foi possível buscar as tarefas" });
+    }
+
+
+
+    
   };
   create = ({ body: { descricao } }, res) => {
     if (!descricao) {
--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const port = 3000;
 app.use(express.json());
 app.use("/tarefas", tarefaRoutes);
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Hello Kitty se escondeu atrás da porta ${port}`);
 });
